4b930fdaba3a6a4f1b9027f26e63b64e4ac353b8
Fkimi <fuccer134097@gmail.com>
2025-01-07 18:06:48 +0900

    Update profile_table.sql

--- profile_table.sql
+++ profile_table.sql
@@ -2,10 +2,10 @@
 -- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- ホスト: localhost
--- 生成日時: 2025 年 1 月 06 日 15:46
--- サーバのバージョン： 10.4.28-MariaDB
--- PHP のバージョン: 8.2.4
+-- ホスト: mysql3104.db.sakura.ne.jp
+-- 生成日時: 2025 年 1 月 07 日 18:05
+-- サーバのバージョン： 8.0.40
+-- PHP のバージョン: 8.2.16
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- データベース: `balubo_db`
+-- データベース: `fuuu_profile_table`
 --
 
 -- --------------------------------------------------------
@@ -28,9 +28,9 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `profile_table` (
-  `id` int(12) NOT NULL,
-  `email` varchar(255) NOT NULL,
-  `pw` varchar(18) NOT NULL,
+  `id` int NOT NULL,
+  `email` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `pw` varchar(18) COLLATE utf8mb4_general_ci NOT NULL,
   `created_at` datetime NOT NULL,
   `updated_at` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
@@ -44,7 +44,10 @@ INSERT INTO `profile_table` (`id`, `email`, `pw`, `created_at`, `updated_at`) VA
 (2, 'SQL練習', 'SQL練習', '2025-01-03 17:42:14', '2025-01-03 17:42:14'),
 (3, 'SQL練習', 'SQL練習', '2025-01-03 18:20:27', '2025-01-03 18:20:27'),
 (20, 'test@aaa', '111', '2025-01-06 23:45:54', '2025-01-06 23:45:54'),
-(21, 'test@aaa', 'vfgs', '2025-01-06 23:46:01', '2025-01-06 23:46:01');
+(21, 'test@aaa', 'vfgs', '2025-01-06 23:46:01', '2025-01-06 23:46:01'),
+(22, 'tesut@a', 'a', '2025-01-07 17:45:59', '2025-01-07 17:45:59'),
+(23, 'a@d', 'd', '2025-01-07 17:46:13', '2025-01-07 17:46:13'),
+(24, 'a@a', 'vfgs', '2025-01-07 18:00:07', '2025-01-07 18:00:07');
 
 --
 -- ダンプしたテーブルのインデックス
@@ -64,7 +67,7 @@ ALTER TABLE `profile_table`
 -- テーブルの AUTO_INCREMENT `profile_table`
 --
 ALTER TABLE `profile_table`
-  MODIFY `id` int(12) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
